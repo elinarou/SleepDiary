df6f99a51e7903f056e41659def7625dd241fd3e
elinarou <elina_rouvinen@outlook.com>
2023-04-26 18:53:33 +0300

    EntryScreen in progress

--- components/screens/EntryScreen.js
+++ components/screens/EntryScreen.js
@@ -1,18 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import Rating from '../Rating';
 
 export default function EntryForm() {
+  const [entry, setEntry] = useState(
+    {
+      date: new Date(),
+      bedTime: '',
+      sleepOnset: '',
+      //sleepDelay: '',
+      awake: false,
+      awakeTime: '',
+      sleepEnd: '',
+      //sleepTime: '',
+      comment: '',
+      quality: ''  
+    });
+
+  const saveEntry = () => {
+
+  };
 
   return (
     <View style={styles.container}>
       <Text>When did you get into bed?</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setEntry({...entry, bedTime: text})}
+        value={entry.bedTime}
+        keyboardType="numeric"
+        placeholder='HH:mm'
+      />
       <Text>When did you fall asleep?</Text>
-      <Text>How many times did you wake up during the nigh?</Text>
-      <Text>1. Wake-up: How long you were awake?</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setEntry({...entry, sleepOnset: text})}
+        value={entry.sleepOnset}
+        keyboardType="numeric"
+        placeholder='HH:mm'
+      />
+      <Text>Did you wake up during the nigh?</Text>
+      <Button
+        onPress={() => setEntry({...entry, awake: true})}
+        title="Yes"
+      />
+      {entry.awake &&
+        (<View>
+          <Text>How long were you awake in total?</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setEntry({...entry, awakeTime: text})}
+            value={entry.awakeTime}
+            keyboardType="numeric"
+            placeholder='Min'
+          />
+        </View>)
+      }
       <Text>When did you wake up?</Text>
-      <Text>When did you get out of bed?</Text>
-      <Text>How would you rate your sleep quality?</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setEntry({...entry, sleepEnd: text})}
+        value={entry.sleepEnd}
+        placeholder='HH:mm'
+      />
       <Text>Other comments?</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setEntry({...entry, comment: text})}
+        value={entry.comment}
+        placeholder='Comment'
+        multiline
+        numberOfLines={4}
+        maxLength={250}
+        textAlignVertical='top'
+      />
+      <Text>How would you rate your sleep quality?</Text>
+      <Rating />
+      <Button
+        onPress={saveEntry()}
+        title="Save"
+      />
     </View>
   ); 
 }
@@ -24,6 +92,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    padding: 5,
+    margin: 10
+  },
+
   button: {
     
   }
