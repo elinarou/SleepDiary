8bfa8e859e718da669e44a089dfaa37cd08b3c28
elinarou <elina_rouvinen@outlook.com>
2023-05-06 00:11:36 +0300

    Modified style

--- components/screens/DiaryScreen.js
+++ components/screens/DiaryScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
+import LatestEntry from '../contents/LatestEntry';
 
 
 export default function DiaryScreen({ navigation }) {
 
   return (
     <View style={styles.button}>
+      <LatestEntry />
       <Button 
         onPress={() => navigation.navigate("Entry")}
         title="Make an entry"
@@ -21,8 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
   button: {
-    
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
   
