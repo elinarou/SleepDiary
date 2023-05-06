ae965f28b4c42570124059c4ec2f1736a0bff145
elinarou <elina_rouvinen@outlook.com>
2023-05-06 23:54:35 +0300

    LatestEntry in progress

--- components/contents/LatestEntry.js
+++ components/contents/LatestEntry.js
@@ -2,36 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { onValue, ref, query, limitToLast } from 'firebase/database';
 import { database } from '../database/FirebaseConfig';
-import FormatDate from '../functions/FormatDate';
+import FormatDateTime from '../functions/FormatDateTime';
 import FormatMinutes from '../functions/FormatMinutes';
 
 
 export default function LatestEntry() {
+  const [showLatest, setShowLatest] = useState(false);
   const [latest, setLatest] = useState([]);
 
   useEffect(() => {
     const entriesRef = query(ref(database, 'entries/'), limitToLast(1));
     onValue(entriesRef, (snapshot) => {
-      const data = snapshot.val();
-      setLatest(Object.values(data));
+      if (snapshot.val() !== null) {
+        const data = snapshot.val();
+        setLatest(Object.values(data));
+        setShowLatest(true);
+      } 
+      else {
+        setShowLatest(false);
+      };
     })
   }, []);
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Your latest entry</Text>
-      <FlatList
-      style={styles.list}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({item}) => 
-        <View style={styles.listcontainer}>
-          <Text>You went to bed <FormatDate value={new Date(item.bedTime)} /></Text>
-          <Text>You woke up <FormatDate value={new Date(item.sleepEnd)} /></Text>              
-          <Text>Your latest sleep lasted for <FormatMinutes value={item.sleepTime} /></Text>
-        </View>}
-      data={latest}
-    /> 
+      {showLatest ?
+        <View>
+          <Text style={styles.heading}>Your latest entry</Text>
+          <FlatList
+            style={styles.list}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) => 
+              <View style={styles.listcontainer}>
+                <Text style={styles.heading}>{item.entryDate}</Text>
+                <Text>Bedtime: <FormatDateTime value={new Date(item.bedTime)} /></Text>
+                <Text>Awakening: <FormatDateTime value={new Date(item.sleepEnd)} /></Text>              
+                <Text>Sleep time: <FormatMinutes value={item.sleepTime} /></Text>
+                <Text>Sleep latency: {item.sleepDelay}</Text>
+                <Text>Awake time: {item.awakeTime}</Text>
+                <Text>Quality: {item.quality}</Text>
+                <Text>Comment: {item.comment}</Text>
+              </View>}
+            data={latest}
+          />
+        </View>
+        : <Text style={styles.heading}>Start your Sleep Diary</Text> 
+      }
     </View>
   );    
 }
