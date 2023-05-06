c8dc92753c73530a7b61560947e0453041081cf7
elinarou <elina_rouvinen@outlook.com>
2023-05-06 23:57:21 +0300

    EntryScreen in progress

--- components/screens/EntryScreen.js
+++ components/screens/EntryScreen.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import AddEntry from '../contents/AddEntry';
 import { database } from '../database/FirebaseConfig';
 import { push, ref } from 'firebase/database';
 
 
 export default function EntryScreen() {
+  const [showFeedback, setShowFeedback] = useState(false);
 
   // Pushes entry to the database
   const saveEntry = (entry) => {
     push(
       ref(database, 'entries/'), { 
+        'entryDate': entry.entryDate,
         'bedTime': entry.bedTime.toString(), 
         'sleepDelay': entry.sleepDelay,
         'awakeTime': entry.awakeTime,
         'sleepEnd': entry.sleepEnd.toString(),
         'sleepTime': entry.sleepTime,
         'comment': entry.comment,
-        'quality': entry.quality 
+        'quality': entry.quality,
       })
     .then(() => {
       // Saved successfully
-      alert("Entry saved");
+      setShowFeedback(true);
     })  
     .catch((error) => {
       // Save failed
@@ -31,7 +33,11 @@ export default function EntryScreen() {
 
   return (
     <View style={styles.container}>
-      <AddEntry saveEntry={saveEntry} />
+      {showFeedback ?
+        <Text style={styles.heading}>Entry saved</Text>
+        :
+        <AddEntry saveEntry={saveEntry} />
+      }
     </View>
   ); 
 };
@@ -44,5 +50,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 5,
+  },
 });
   
