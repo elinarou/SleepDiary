106661bc37c08626069ffe831a964938d013f5cd
elinarou <elina_rouvinen@outlook.com>
2023-05-09 20:30:25 +0300

    Changed name

--- components/functions/CalculateAvg.js
+++ components/functions/CalculateAvg.js
@@ -1,6 +1,6 @@
 export default function CalculateAvgMin(props) {
   const average = props.arr.reduce((a, b) => a + b, 0) / props.arr.length;
 
-  return `${average.toFixed()} min`;
+  return average.toFixed();
 };
 
