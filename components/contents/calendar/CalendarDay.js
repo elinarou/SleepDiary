125ce0b5b297cedfc2a4a415da10649480b77980
elinarou <elina_rouvinen@outlook.com>
2023-05-09 20:26:46 +0300

    Added deleteEntry

--- components/contents/calendar/CalendarDay.js
+++ components/contents/calendar/CalendarDay.js
@@ -1,28 +1,52 @@
 import React, {useState} from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { ref, query, equalTo, orderByChild, remove, onValue, child, key, limitToLast } from 'firebase/database';
+import { database } from '../../database/FirebaseConfig';
 import FormatDateTime from '../../functions/FormatDateTime';
 import FormatMinutes from '../../functions/FormatMinutes';
+import { TouchableOpacity } from 'react-native';
 
 
 export default function SleepCalendarDay(props) {
 
+  const deleteEntry = () => {
+    const entriesRef = query(ref(database, 'entries/'), 
+      orderByChild('entryDate'), 
+      equalTo(props.selectedDate));
+    // Finds and deletes key reference
+    onValue(entriesRef, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        remove(ref(database, 'entries/' + childKey));
+      });
+    });  
+  };
+
   return (
     <View style={styles.container}>
-      {props.showEntry ?
-      <FlatList
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({item}) => 
-          <View style={styles.listcontainer}>
-            <Text style={styles.text}>Bedtime: <FormatDateTime value={new Date(item.bedTime)} /></Text>
-            <Text style={styles.text}>Awakening: <FormatDateTime value={new Date(item.sleepEnd)} /></Text>              
-            <Text style={styles.text}>Sleep time: <FormatMinutes value={item.sleepTime} /></Text>
-            <Text style={styles.text}>Sleep latency: {item.sleepDelay} min</Text>
-            <Text style={styles.text}>Awake time: {item.awakeTime} min</Text>
-            <Text style={styles.text}>Sleep quality: {item.quality}/5</Text>
-            <Text style={styles.text}>Comment: {item.comment}</Text>
-          </View>}
-        data={props.entries}
-      />:
+      {props.showEntry ? // Conditional rendering, 1. & 2.
+      // 1. Entry info
+      <View>
+        <FlatList
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => 
+            <View style={styles.listcontainer}>
+              <Text style={styles.text}>Bedtime: <FormatDateTime value={new Date(item.bedTime)} /></Text>
+              <Text style={styles.text}>Awakening: <FormatDateTime value={new Date(item.sleepEnd)} /></Text>              
+              <Text style={styles.text}>Sleep time: <FormatMinutes value={item.sleepTime} /></Text>
+              <Text style={styles.text}>Sleep latency: {item.sleepDelay} min</Text>
+              <Text style={styles.text}>Awake time: {item.awakeTime} min</Text>
+              <Text style={styles.text}>Sleep quality: {item.quality}/5</Text>
+              <Text style={styles.text}>Comment: {item.comment}</Text>
+            </View>}
+          data={props.entries}
+        />
+        <TouchableOpacity style={styles.button}
+          onPress={() => deleteEntry()}>
+          <Text style={styles.heading}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+      : // 2. No entry found
       <Text style={styles.heading}>No entry</Text>
       }
     </View>
@@ -39,6 +63,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     fontSize: 15
    },
+
+   button: {
+    padding: 5,
+    backgroundColor: 'gray'
+  },
 
    heading: {
     flex: 1,
